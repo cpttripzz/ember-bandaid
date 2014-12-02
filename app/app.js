7056e111abab7924c1425d3fc9533f825ff75ec7
cpttripzz <zach.erskine@gmail.com>
2014-12-03 00:10:49 +0200

    injections

--- app/app.js
+++ app/app.js
@@ -8,10 +8,32 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  apiPath: 'http://bandaid.com'
+});
+
+App.Constant = Ember.Object.extend({
+
+    path: 'http://bandaid.com'
+
+
 });
 
 
+App.initializer({
+  name: 'const',
+  initialize: function(container, application) {
+    application.register('constants:main', App.Constant, {
+      singleton: true,
+      instantiate: true
+    });
+
+    application.inject('controller', 'const', 'constants:main');
+    application.inject('view', 'const', 'constants:main');
+    application.inject('model', 'const', 'constants:main');
+    application.inject('route', 'const', 'constants:main');
+  }
+});
 loadInitializers(App, config.modulePrefix);
 
 export default App;
--- app/models/genre.js
+++ app/models/genre.js
@@ -1,5 +1,12 @@
 import DS from "ember-data";
+import Ember from 'ember';
+
 export default DS.Model.extend({
     name: DS.attr('string'),
     slug: DS.attr('string'),
+    genreImgUrl: function() {
+        return this.const.path +'/media/cache/thumb/img/genres/' + this.get('slug') + '.jpg';
+
+    }.property('src')
 });
+
